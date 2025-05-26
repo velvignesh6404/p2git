734f77a417108f477836de48c8b1d6a730148b9c
Velvignesh Pillai <141361652+velvignesh6404@users.noreply.github.com>
2025-05-26 23:28:07 +0530

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -1,5 +1,5 @@
 public class Sample{
   public static void main(String[]args){
-    System.out.println("Java programming code by Velvignesh Pillai")
+    System.out.println("Java programming code by Velvignesh Pillai");
   }
 }
